17e73f88d1913bb2837bbfbc3148b5b09d7b2d7f
Damian <damianpietron007@gmail.com>
2024-10-09 10:28:37 +0200

    Create Calculator.java, implement substracting and adding methodology based on int type

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,13 @@
+package calculator.src.main.java.com.example;
+
+public class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    
+}
