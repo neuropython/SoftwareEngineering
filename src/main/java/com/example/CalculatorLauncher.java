57c877b847be0707fe3b46b29dc65f90e743c869
Damian <damianpietron007@gmail.com>
2024-10-09 10:15:01 +0200

    Initial commit

---
+++ src/main/java/com/example/CalculatorLauncher.java
@@ -0,0 +1,13 @@
+package calculator.src.main.java.com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class CalculatorLauncher 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Comming Soon!" );
+    }
+}
